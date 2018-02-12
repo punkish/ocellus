7801b5b88955ac6f52a79988d7635cc231327387
punkish <punk.kish@gmail.com>
2018-02-12 21:08:08 +0100

    refactored fancysearch with inline help

--- js/fancysearch.js
+++ js/fancysearch.js
@@ -1,60 +1,154 @@
 /*
-    JavaScript fancySearch
-    created by P Kishor
-    GitHub: https://github.com/punkish/fancysearch
-    License: Released into the public domain under CC0
+JavaScript fancySearch
+created by P Kishor
+GitHub: https://github.com/punkish/fancysearch
+License: Released into the public domain under CC0
 */
 
-"use strict";
+'use strict';
 
+/**
+ * Represents an instance of fancySearch.
+ * 
+ * <div id="fs-container">
+ *      <div id="fs-help">some help</div>
+ *      <div id="fs-widget">
+ *          <div id="fs-params">
+ * 
+ *              <!-- the following repeats as needed -->
+ *              <div class="fs-param-empty|fs-param-filled">
+ *                  <div class="fs-key on|off"></div>
+ *                  <input class="fs-key-input on|off">
+ *                  <div class="fs-val on|off"></div>
+ *                  <input class="fs-val-input on|off">
+ *                  <div class="fs-cancel off"></div>
+ *              </div>
+ *              <!-- end div.fs-param-empty -->
+ * 
+ *          </div>
+ *          <button id="fs-go" type="submit" class="fs-button-primary">go</button>
+ *      </div>
+ *      <div class="fs-throbber"></div>
+ * </div>
+ * 
+ */
 const fancySearch = function() {
 
-    // fsDiv is the container in which fancySearch is created
-    // Everything happens inside fsDiv. It is set during init()
-    let fsDiv;
+    let fsHelp;
+    let fs;
+    let theFacets;
     let facetKeys;
     let facetValues;
+    let resultCb;
 
-	const makeFsContainer = function() {
+    /**
+     * create an HTML element
+     * 
+     * @constructor
+     * @param {string} elType - The type of element
+     * @param {element} container - HTML element that contains this newly formed element
+     * @param {string} id - id of the new element
+     * @param {string} class - className of the new element
+     * @param {string} type - button-specific type of button property
+     * @param {string} text - innerText of the new element
+     */
+    const elementMaker = function(o) {
+        let element = document.createElement(o.elType);
+        if (o.id) element.id = o.id;
+        if (o.class) element.className = o.class;
+        if (o.type) element.type = o.type;
+        if (o.text) element.innerText = o.text;
+        if (o.html) element.innerHTML = o.html;
 
-        // We are going to create the following HTML
-        /*
-         *
-         * <div class="fs-param-empty">
-         *    <div class="fs-key off"></div>
-         *    <input class="fs-key-input">
-         *    <div class="fs-val off"></div>
-         *    <input class="fs-val-input off">
-         *    <div class="fs-cancel off"></div>
-         * </div>
-         *
-         */
+        o.container.appendChild(element);
+
+        return element;
+    };
+
+    /** 
+     * Creates div#fsContainer which contains the entire fancySearch machinery
+     * @param {string} selector - HTML element that will house fancySearch
+     * 
+     */
+    const makeFsContainer = function(selector) {
+
+        fsHelp = elementMaker({
+            elType: 'div', 
+            id: 'fs-help', 
+            html: 'choose a facet from the dropdown',
+            container: selector
+        });
+
+        let fsw = elementMaker({
+            elType: 'div', 
+            id: 'fs-widget', 
+            container: selector
+        });
+
+        fs = elementMaker({
+            elType: 'div', 
+            id: 'fs', 
+            class: 'fs', 
+            container: fsw
+        });
+
+        let fsb = elementMaker({
+            elType: 'button', 
+            id: 'fs-go', 
+            type: 'submit', 
+            class: 'fs-button-primary', 
+            text: 'go', 
+            container: fsw
+        });
+
+        fsb.addEventListener('click', function(event) {
+            event.preventDefault();
+            event.stopPropagation();
     
-        let fsp = document.createElement('div');
-        fsp.className = 'fs-param-empty';
+            result(resultCb);
+        });
+
+        makeFsKvElements();
+    };
+
+    const makeFsKvElements = function() {
+
+        let fsp = elementMaker({
+            elType: 'div', 
+            class: 'fs-param-empty',
+            container: fs
+        });
     
-        let fsk = document.createElement('div');
-        fsk.className = 'fs-key fs-off';
-        let fskInput = document.createElement('input');
-        fskInput.className = 'fs-key-input';
+        elementMaker({
+            elType: 'div', 
+            class: 'fs-key fs-off',
+            container: fsp
+        });
+
+        let fskInput = elementMaker({
+            elType: 'input', 
+            class: 'fs-key-input',
+            container: fsp
+        });
     
-        let fsv = document.createElement('div');
-        fsv.className = 'fs-val fs-off';
-        let fsvInput = document.createElement('input');
-        fsvInput.className = 'fs-val-input fs-off';
+        elementMaker({
+            elType: 'div', 
+            class: 'fs-val fs-off',
+            container: fsp
+        });
+
+        let fsvInput = elementMaker({
+            elType: 'input', 
+            class: 'fs-val-input fs-off',
+            container: fsp
+        });
     
-        let fsc = document.createElement('div');
-        fsc.innerText = 'x';
-        fsc.className = 'fs-cancel fs-off';
-    
-        // add '.fs-key' div to '.fs-param' div
-        // and then add '.fs-param' div to 'fs' div
-        fsp.appendChild(fsk);
-        fsp.appendChild(fskInput);
-        fsp.appendChild(fsv);
-        fsp.appendChild(fsvInput);
-        fsp.appendChild(fsc);
-        fsDiv.appendChild(fsp);
+        let fsc = elementMaker({
+            elType: 'div', 
+            class: 'fs-cancel fs-off',
+            text: 'x',
+            container: fsp
+        });
     
         fskInput.addEventListener('keydown', modifyKeyPress);
         fsvInput.addEventListener('keydown', modifyKeyPress);
@@ -90,7 +184,7 @@ const fancySearch = function() {
                 fsvInput.className = 'fs-off';
                 fsc.className = 'fs-cancel fs-on';
     
-                makeFsContainer();
+                makeFsKvElements();
             }
     
             event.preventDefault();
@@ -127,35 +221,44 @@ const fancySearch = function() {
         const that = this.parentElement;
         that.parentElement.removeChild(that);
     };
-    
+
     const aCfacets = function(type, selector, choices) {
-    
+        
         new autoComplete({
             selector: selector,
             minChars: Array.isArray(choices) ? 0 : 3,
             source: function(term, response) {
     
                 if (Array.isArray(choices)) {
-                    term = term.toLowerCase();
-                    let matches = [];
-                    let j = choices.length;
-        
-                    for (let i = 0; i < j; i++) {
-                        if (~choices[i].toLowerCase().indexOf(term)) {
-                            matches.push(choices[i]);
+                    if (choices.length > 0) {
+                        term = term.toLowerCase();
+                        let matches = [];
+                        let j = choices.length;
+            
+                        for (let i = 0; i < j; i++) {
+                            if (~choices[i].toLowerCase().indexOf(term)) {
+                                matches.push(choices[i]);
+                            }
                         }
+
+                        response(matches);
                     }
-        
-                    response(matches);
                 }
                 else {
-                    try { xhr.abort(); } catch(e){}
+                    try {
+                        xhr.abort();
+                    } 
+                    catch(e) {
+                        
+                    }
                     xhr(choices + '/' + term, response);
                 }
             },
+
             onSelect: function(e, term, item) {
     
                 let newChoices;
+                let prompt;
                 let thisInput = this.selector;
                 let fsp = thisInput.parentElement;
                 
@@ -172,6 +275,7 @@ const fancySearch = function() {
                     for (; i < j; i++) {
                         if (facets[i]['key'] === term) {
                             newChoices = facets[i]['values'];
+                            prompt = facets[i]['prompt'];
                             if (facets[i]['noDuplicates']) {
                                 facetKeys.splice(facetKeys.indexOf(term), 1);
                             }
@@ -181,10 +285,13 @@ const fancySearch = function() {
     
                     let nextInput = fsp.children[3];
                     nextInput.className = 'fs-val-input fs-on';
+                    fsHelp.innerHTML = prompt;
+                    
                     aCfacets('val', nextInput, newChoices);
                     nextInput.focus();
                 }
                 else if (type === 'val') {
+                    fsHelp.innerHTML = 'choose a facet from the dropdown';
     
                     let fsv = fsp.children[2];
                     
@@ -207,7 +314,7 @@ const fancySearch = function() {
                         }
                     }
     
-                    makeFsContainer();
+                    makeFsKvElements();
                 }
             }
         });
@@ -234,46 +341,59 @@ const fancySearch = function() {
         x.send();
     };
 
-	return  {
+    /**
+     * Package content of fs into a JSON object
+     * @param {*} cb 
+     */
+    const result = function(cb) {
         
-        result: function() {
-        
-            const params = fs.childNodes;
-            let q = {};
-            for (let i = 0, j = params.length; i < j; i++) {
-                const kv = params[i].children;
-        
-                // https://stackoverflow.com/questions/952924/javascript-chop-slice-trim-off-last-character-in-string
-                const key = kv[1].value;
-                const val = kv[3].value.replace(/\n$/, '');
-        
-                if (key && val) {
-                    if (key in q) {
-                        if (typeof(q[key]) === 'string') {
-                            const str = q[key];
-                            q[key] = [str, val]
-                        }
-                        else {
-                            q[key].push(val);
-                        }
+        const params = fs.childNodes;
+        let q = {};
+        for (let i = 0, j = params.length; i < j; i++) {
+            const kv = params[i].children;
+    
+            // https://stackoverflow.com/questions/952924/javascript-chop-slice-trim-off-last-character-in-string
+            const key = kv[1].value;
+            const val = kv[3].value.replace(/\n$/, '');
+    
+            if (key && val) {
+                if (key in q) {
+                    if (typeof(q[key]) === 'string') {
+                        const str = q[key];
+                        q[key] = [str, val]
                     }
                     else {
-                        q[key] = val;
+                        q[key].push(val);
                     }
                 }
+                else {
+                    q[key] = val;
+                }
             }
-            
-            return q;
-        },
+        }
+        
+        //return q;
+        cb(q);
+    };
 
-		init: function(selector, facets) {
+	return  {
+
+        /**
+         * create an instance of fancySearch.
+         * @constructor
+         * @param {div} selector - The div where fancySearch should be initiated.
+         * @param {array} facets - The facets to use for this instance of fancySearch.
+         * @param {func} doSomethingWithQuery - Ingest the JSON object with fancySearch selection, and do something with it.
+         */
+		init: function(selector, facets, doSomethingWithQuery) {
             
             // https://stackoverflow.com/questions/7028145/find-key-name-in-hash-with-only-one-key
+            theFacets = facets;
             facetKeys = facets.map(element => { return element['key'] });
             facetValues = facets.map(element => { return element['values'] });
-            fsDiv = selector;
+            resultCb = doSomethingWithQuery;
             
-            makeFsContainer();
+            makeFsContainer(selector);
 		}
 	};
 };
