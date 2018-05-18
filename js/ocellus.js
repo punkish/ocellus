03b098430eea77cdb1be0a9bf89604f0828d1f8d
punkish <punk.kish@gmail.com>
2018-05-18 08:31:48 -0400

    deactivated carousel, for now

--- js/ocellus.js
+++ js/ocellus.js
@@ -7,6 +7,7 @@ const zenodoRecord = 'https://zenodo.org/record/';
 
 let tnsource = 'zenodo';
 let defaultLayout = 'masonry';
+let htmlCarouselStr = '';
 
 // figcaption height
 const fch = '30px';
@@ -14,7 +15,7 @@ let figcaptions = [];
 let fclength;
 
 let els = {};
-["numOfFoundRecords","grid","masonry","cacheMsg","qReqdMsg","cacheMsgCheck","q","qWrapper","throbber","btnImages","aboutLink","about","closeAbout","prev","next","pager","wrapper","html","footer"].forEach(function(el) {
+["numOfFoundRecords","grid","masonry","cacheMsg","qReqdMsg","cacheMsgCheck","q","qWrapper","throbber","btnImages","aboutLink","about","closeAbout","prev","next","pager","wrapper","html","footer", "carouselbox", "carousel"].forEach(function(el) {
     els[el] = document.getElementById(el);
 });
 
@@ -66,12 +67,98 @@ const getImagesFromButton = function(event) {
         1,                             // page
         els['cacheMsgCheck'].checked,  // refreshCache
         tnsource,
-        layout
+        defaultLayout
     );
 };
 
+const carousel = function() {
+    //const re = '(.*?)(250,)(.*)';
+    // const thumb960 = thumb250.replace('250,', '960,');
+    // let html = '<figure class="item">';
+    // html += `<a href="${image}" target="_blank"><img class="z" src="${thumb960}"></a>`;
+    // html += `<figcaption class="transition-050 opacity85 text">
+    //                 rec ID: <a class="transition-050">${recId}</a>
+    //                 <div class="transition-050 desc">${title}. <a href='${zenodoRecord}${recId}' target='_blank'>more</a></div>
+    //             </figcaption>
+    //         </figure>`;
+
+    els['carousel'].innerHTML = htmlCarouselStr;
+    els['carouselbox'].className = 'on';
+    lazyload();
+    els['masonry'].className = 'off';
+    els['grid'].className = 'off';
+    els['pager'].className = 'off';
+    
+    // Read necessary elements from the DOM once
+    //var box = document.querySelector('#carouselbox');
+    var box = document.getElementById('carouselbox');
+    var next = box.querySelector('.next');
+    var prev = box.querySelector('.prev');
+
+    // Define the global counter, the items and the 
+    // current item 
+    var counter = 0;
+    var items = box.querySelectorAll('.content figure');
+    var amount = items.length;
+    var current = items[0];
+    // let thumb960 = current.childNodes[0].firstChild.src.replace('250,', '960,');
+    //current.childNodes[0].firstChild.src = current.childNodes[0].firstChild.dataset.src;
+
+    box.classList.add('active');
+
+    // navigate through the carousel
+
+    function navigate(direction) {
+
+        // hide the old current list item 
+        current.classList.remove('current');
+        
+        // calculate th new position
+        counter = counter + direction;
+    
+        // if the previous one was chosen
+        // and the counter is less than 0 
+        // make the counter the last element,
+        // thus looping the carousel
+        if (direction === -1 && counter < 0) { 
+            counter = amount - 1; 
+        }
+    
+        // if the next button was clicked and there 
+        // is no items element, set the counter 
+        // to 0
+        if (direction === 1 && !items[counter]) { 
+            counter = 0;
+        }
+    
+        // set new current element 
+        // and add CSS class
+        current = items[counter];
+        // thumb960 = current.childNodes[0].firstChild.src.replace('250,', '960,');
+        //current.childNodes[0].firstChild.src = current.childNodes[0].firstChild.dataset.src;
+        current.classList.add('current');
+    }
+
+    // add event handlers to buttons
+    next.addEventListener('click', function(ev) {
+        navigate(1);
+    });
+    prev.addEventListener('click', function(ev) {
+        navigate(-1);
+    });
+
+    // show the first element 
+    // (when direction is 0 counter doesn't change)
+    navigate(0);
+
+};
+
 const makeLayout = function(imagesOfRecords) {
-    let html = '';
+    //let html = '';
+    let htmlCarousel = '';
+    let htmlGrid = '';
+    let thumb960 = '';
+
     let imgCount = 0;
     //let recordCount = 0;
     //const imagesOfRecords = data.imagesOfRecords;
@@ -83,31 +170,45 @@ const makeLayout = function(imagesOfRecords) {
         const title = imagesOfRecords[record]["title"];
         const creators = imagesOfRecords[record]["creators"];
         const thumb250 = imagesOfRecords[record]["thumb250"];
+        const recId = record.split('/').pop();
 
         const j = images.length;
         imgCount = imgCount + j;
 
-        html += "<figure class='item'>";
+        htmlCarousel += "<figure>";
+        htmlGrid += "<figure class='item'>";
+
 
         for (let i = 0; i < j; i++) {
             if (tnsource === 'zenodo') {
-                html += `<a href="${images[i]}" target="_blank"><img class="z" src="${thumb250}"></a>`;
+                htmlGrid += `<a href="${images[i]}" target="_blank"><img class="z" src="${thumb250}"></a>`;
+                //htmlGrid += `<a href="#carousel" onclick="carousel();"><img class="z" src="${thumb250}"></a>`;
+
+                thumb960 = thumb250.replace('250,', '960,');
+                //htmlCarousel += `<a href="${images[i]}" target="_blank"><img data-frz-src="${thumb960}" src="${thumb250}" onload="lzld(this);" onerror="lzld(this);"></a>`;
+                //htmlCarousel += `<a href="${images[i]}" target="_blank"><img data-src="${thumb960}" src="${thumb250}"></a>`;
+
+                htmlCarousel += `<a href="${images[i]}" target="_blank"><img class="lazyload" src="${thumb250}" data-src="${thumb960}"></a>`;
+
             }
             else {
-                html += `<a href="${images[i]}" target="_blank"><img class="z" src="${imageresizer}${images[i].replace(zenodoApi, '')}"></a>`;
+                htmlGrid += `<a href="${images[i]}" target="_blank"><img class="z" src="${imageresizer}${images[i].replace(zenodoApi, '')}"></a>`;
             }
         }
 
-        const recId = record.split('/').pop();
-
-        html += `<figcaption class="transition-050 opacity85 text">
+        htmlGrid += `<figcaption class="transition-050 opacity85 text">
                     rec ID: <a class="transition-050">${recId}</a>
                     <div class="transition-050 desc">${title}. <a href='${zenodoRecord}${recId}' target='_blank'>more</a></div>
                 </figcaption>
             </figure>`;
+
+        htmlCarousel += `<figcaption>
+                    rec ID: <b>${recId}.</b> ${title}. <a href='${zenodoRecord}${recId}' target='_blank'>more</a></div>
+                </figcaption>
+            </figure>`;
     }
 
-    return [html, imgCount];
+    return [htmlGrid, htmlCarousel, imgCount];
     //return html;
 };
 
@@ -145,7 +246,6 @@ const getImages = function(event, qry, page, refreshCache, tnsource, layout) {
         }
     }
     
-
     if (!layout) {
         layout = defaultLayout;
     }
@@ -164,13 +264,15 @@ const getImages = function(event, qry, page, refreshCache, tnsource, layout) {
             if (x.status === 200) {
                 var data = JSON.parse(x.responseText);
                 if (data.total) {
-                    const [html, imgCount] = makeLayout(data.result);
+                    const [htmlGrid, htmlCarousel, imgCount] = makeLayout(data.result);
                     //const html = makeLayout(data.imagesOfRecords);
                     //els['grid'].innerHTML = html;
                 
                     els['numOfFoundRecords'].innerHTML = `${data.total} records found`;
                     els['numOfFoundRecords'].className = 'on';
-                    els[layout].innerHTML = html;
+                    els[layout].innerHTML = htmlGrid;
+                    // els['carousel'].innerHTML = htmlCarousel;
+                    htmlCarouselStr = htmlCarousel;
 
                     if (imgCount >= 30) {
                         els['prev'].href = (page === 1) ? '?' + qStr1 + 1 : '?' + qStr1 + (page - 1);
