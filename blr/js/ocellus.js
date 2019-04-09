4f6be39042210e9830129686fb989a47dc00d59f
punkish <punk.kish@gmail.com>
2019-04-09 13:56:29 +0200

    working blr

--- blr/js/ocellus.js
+++ blr/js/ocellus.js
@@ -3,13 +3,10 @@
 const Ocellus = (function() {
     
     // private stuff
-    let zenodeo = 'https://zenodeo.punkish.org';
-    const basePath = '/v2/records?size=30&communities=biosyslit&access_right=open&type=image&summary=false&images=true&';
-    let baseUrl;
+    const basePath = '/v2/records?size=30&communities=biosyslit&access_right=open&type=image&';
     const zenodoApi = 'https://zenodo.org/api/files/';
     const zenodoRecord = 'https://zenodo.org/record/';
     
-    let layout = 'masonry';
 
     // figcaption settings //////////////////
     let figcaptionHeight = '30px';
@@ -25,19 +22,31 @@ const Ocellus = (function() {
         next: ''
     };
 
-    let qField;
-    let qValue;
-    let templateMasonry;
-    let templateCarousel;
-    let carouselContainer;
-    let carousel;
-    let wrapper;
+    let zenodeo;
+    let layout;
+    let baseUrl;
+
+    let ssGo;
+    let ssForm;
+
+    let q;
+    let resultType;
+    let communities;
+    let refreshCache;
+    //let qValue;
+
+    let resultTypeChooser;
     let throbber;
     let footer;
+    let wrapper;
+    let carousel;
+    let carouselContainer;
     let about;
     let aboutLink;
     let aboutClose;
-    let ssGo;
+
+    let templateMasonry;
+    let templateCarousel;
 
     // we use these when toggling about
     let wrapperState;
@@ -57,7 +66,7 @@ const Ocellus = (function() {
     const getQueryParamsAndImages = function(search) {
 
         const qryStr = getQueryStr(search);
-        qValue = qryStr['q'];
+        //qValue = qryStr['q'];
     
         getImages(
             qryStr['q'],            // qry 
@@ -110,42 +119,55 @@ const Ocellus = (function() {
         return [figures, imgCount];
     };
     
-    const getImages = function(qValue, page, refreshCache) {
+    const getImages = function(opts) {
+
+        const q = opts.q;
+        const resultType = opts.resultType;
+        const page = opts.page;
+        const refreshCache = opts.refreshCache;
+
+        const method = 'GET';
         
         // we attach qStr1 to 'prev' and 'next' links 
         // with the correctly decremented or  
         // incremented page number
-        let qStr1 = 'q=' + qValue;
+        let qStr1 = `size=30&communities=biosyslit&access_right=open&type=image&q=${q}`;
         
-        if (refreshCache) {
-            qStr1 = qStr1 + '&refreshCache=' + refreshCache;
+        if (refreshCache === 'true') {
+            qStr1 += `&refreshCache=${refreshCache}`;
         }
     
-        qStr1 = qStr1 + '&page=';
-    
-        // qStr2 is used for `history`
-        let qStr2 = qStr1 + page;
-    
-        const method = 'GET';
+        // qStr2 is used for `history` and the address
+        // shown in the browser's URL field
+        let qStr2;
+
         // the complete url to the api
-        let url = baseUrl + qStr2;
+        let url;
+        qStr1 = qStr1 + '&page=';
+        qStr2 = qStr1 + page;
+        url = zenodeo + '/v2/records?' + qStr2;
+
+        history.pushState('', '', '?' + qStr2);
+
+        let qStr3 = `resultType=${resultType}&${qStr1}`
+
         const callback = function(xh) {
 
-            const res = JSON.parse(xh.responseText);
+            const res = JSON.parse(xh.responseText).value;
 
             if (res.total) {
-                const [figures, imgCount] = makeLayout(res.result);
+                const [figures, imgCount] = makeLayout(res.images);
                 data.figures = figures;
                 data.numOfFoundRecords = niceNumbers(res.total);
 
                 if (imgCount >= 30) {
-                    data.prev = (page === 1) ? '?' + qStr1 + 1 : '?' + qStr1 + (page - 1);
-                    data.next = '?' + qStr1 + (parseInt(page) + 1);
+                    data.prev = (page === 1) ? '?' + qStr3 + 1 : '?' + qStr3 + (page - 1);
+                    data.next = '?' + qStr3 + (parseInt(page) + 1);
                     data.pager = true;
                 }
 
                 footer.className = 'relative';
-                history.pushState('', '', '?' + qStr2);
+                
             }
             else {
                 data.numOfFoundRecords = 'Zero';
@@ -153,7 +175,7 @@ const Ocellus = (function() {
                 footer.className = 'fixed';
             }
 
-            qField.value = qValue;
+            //qField.value = qValue;
             wrapper.innerHTML = Mustache.render(templateMasonry, data);
             wrapper.className = 'on';
             throbber.classList.remove('throbber-on');
@@ -174,6 +196,77 @@ const Ocellus = (function() {
                     // cancellers[i].addEventListener('click', cancelReporter);
                 }
             }
+        };
+        const headers = [
+            {k: "Content-Type", v: "application/json;charset=UTF-8"}
+        ];
+        const payload = '';
+
+        x(method, url, callback, headers, payload);
+        
+        return false;
+    };
+
+    const getTreatments = function(opts) {
+
+        const q = opts.q;
+        const resultType = opts.resultType;
+        const page = opts.page;
+        const refreshCache = opts.refreshCache;
+
+        const method = 'GET';
+        
+        // we attach qStr1 to 'prev' and 'next' links 
+        // with the correctly decremented or  
+        // incremented page number
+        let qStr1 = `q=${q}`;
+        
+        if (refreshCache === 'true') {
+            qStr1 += `&refreshCache=${refreshCache}`;
+        }
+    
+        // qStr2 is used for `history` and the address
+        // shown in the browser's URL field
+        //let qStr2;
+    
+        // the complete url to the api
+        let url;
+        //qStr2 = qStr1;
+        url = zenodeo + '/v2/treatments/?' + qStr1;
+
+        history.pushState('', '', '?' + `resultType=${resultType}&${qStr1}`);
+
+
+        const callback = function(xh) {
+
+            const res = JSON.parse(xh.responseText).value;
+
+            if (res.length) {
+                data.treatments = res;
+                // const [figures, imgCount] = makeLayout(res.images);
+                // data.figures = figures;
+                // data.numOfFoundRecords = niceNumbers(res.total);
+
+                // if (imgCount >= 30) {
+                //     data.prev = (page === 1) ? '?' + qStr1 + 1 : '?' + qStr1 + (page - 1);
+                //     data.next = '?' + qStr1 + (parseInt(page) + 1);
+                //     data.pager = true;
+                // }
+
+                footer.className = 'relative';
+                
+            }
+            else {
+                data.numOfFoundRecords = 'Zero';
+                data.pager = false;
+                footer.className = 'fixed';
+            }
+
+            wrapper.innerHTML = Mustache.render(templateTreatmentsFTS, data);
+            wrapper.className = 'on';
+            throbber.classList.remove('throbber-on');
+            throbber.classList.add('throbber-off');
+
         };
         const headers = [
             {k: "Content-Type", v: "application/json;charset=UTF-8"}
@@ -399,7 +492,98 @@ const Ocellus = (function() {
         else {
             return num;
         }
-    }
+    };
+
+    //+creators.name:/Agosti.*/ +publication_date:[1990 TO 1991} +keywords:taxonomy +title:review
+    const goGetIt = function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        throbber.classList.remove('throbber-off');
+        throbber.classList.add('throbber-on');
+        
+        if (resultType.value === 'images') {
+            getImages({ 
+                q: q.value.toLowerCase(),
+                resultType: resultType.value,
+                page: 1,
+                refreshCache: refreshCache.value
+            });
+        }
+        else if (resultType.value === 'treatments') {
+            getTreatments({ 
+                q: q.value.toLowerCase(),
+                resultType: resultType.value,
+                page: 1,
+                refreshCache: refreshCache.value
+            });
+        }
+    };
+
+    const toggleRefreshCache = function(event) {
+            
+        const toggleClass = event.target.dataset['toggleClass'];
+        const toggleTarget = document.querySelector(event.target.dataset['toggleTarget']);
+
+        if (event.target['checked']) {
+            toggleTarget.classList.remove(toggleClass);
+            toggleTarget.classList.add('on');
+        }
+        else {
+            toggleTarget.classList.remove('on');
+            toggleTarget.classList.add(toggleClass);
+        }
+    };
+
+    const toggleAbout = function(event) {
+
+        if (about.classList.contains('on')) {
+
+            // hide about
+            about.classList.remove('on');
+            about.classList.add('off-none');
+
+            // restore wrapper and footer
+            footer.className = footerState;
+            wrapper.className = wrapperState;
+        }
+        else {
+            
+            // save wrapper and footer state
+            wrapperState = wrapper.className;
+            footerState = footer.className;
+
+            // hide wrapper
+            wrapper.className = 'off-none';
+
+            // make footer fixed
+            footer.className = 'fixed';
+
+            // make about visible
+            about.classList.remove('off-none');
+            about.classList.add('on');
+        }
+    };
+
+    const chooseResultType = function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        
+        // https://stackoverflow.com/questions/4564414/delete-first-character-of-a-string-in-javascript
+        const s = event.target.hash.substr(1);
+        resultType.value = s;
+
+        for (let i = 0; i < resultTypeChooser.children.length; i++) {
+
+            if (s === resultTypeChooser.children[i].hash.substr(1)) {
+                resultTypeChooser.children[i].classList.add('searchFocus');
+            }
+            else {
+                resultTypeChooser.children[i].classList.remove('searchFocus');
+            }
+        }
+        
+    };
 
     // public stuff
     return {
@@ -421,61 +605,9 @@ const Ocellus = (function() {
             wrapper.className = 'on';
         },
 
-        //+creators.name:/Agosti.*/ +publication_date:[1990 TO 1991} +keywords:taxonomy +title:review
-        goGetIt: function(options) {
-            
-            qValue = options.qValue;
-            getImages( 
-                qValue.toLowerCase(), // qry
-                1,                    // page
-                options.refreshCache, // refreshCache
-            );
-        },
+        
 
-        toggleRefreshCache: function(event) {
-            
-            const toggleClass = event.target.dataset['toggleClass'];
-            const toggleTarget = document.querySelector(event.target.dataset['toggleTarget']);
-
-            if (event.target['checked']) {
-                toggleTarget.classList.remove(toggleClass);
-                toggleTarget.classList.add('on');
-            }
-            else {
-                toggleTarget.classList.remove('on');
-                toggleTarget.classList.add(toggleClass);
-            }
-        },
-
-        toggleAbout: function(event) {
-
-            if (about.classList.contains('on')) {
-
-                // hide about
-                about.classList.remove('on');
-                about.classList.add('off-none');
-
-                // restore wrapper and footer
-                footer.className = footerState;
-                wrapper.className = wrapperState;
-            }
-            else {
-                
-                // save wrapper and footer state
-                wrapperState = wrapper.className;
-                footerState = footer.className;
-
-                // hide wrapper
-                wrapper.className = 'off-none';
-
-                // make footer fixed
-                footer.className = 'fixed';
-
-                // make about visible
-                about.classList.remove('off-none');
-                about.classList.add('on');
-            }
-        },
+        
         
         /*
          * Initialization options
@@ -490,41 +622,49 @@ const Ocellus = (function() {
         init: function(options) {
 
             // store the options in private variables for later use
-            if (options.zenodeo) {
-                zenodeo = options.zenodeo;
-            }
-            baseUrl = options.zenodeo + basePath;
+            zenodeo = options.zenodeo;
+            layout = options.layout;
 
-            if (options.layout) {
-                layout = options.layout;
-            }
+            ssGo = options.ssGo;
+            ssForm = options.ssForm;
 
-            if (options.facets) {
-                facets = options.facets;
-            }
+            // if (options.facets) {
+            //     facets = options.facets;
+            // }
 
-            qField = options.qField;
-            templateMasonry = options.templateMasonry;
-            wrapper = options.wrapper;
-            templateCarousel = options.templateCarousel;
-            carouselContainer = options.carouselContainer;
-            carousel = options.carousel;
+            q = options.q;
+            resultType = options.resultType;
+            communities = options.communities;
+            refreshCache = options.refreshCache;
+
+            resultTypeChooser = options.resultTypeChooser;
             throbber = options.throbber;
             footer = options.footer;
+            wrapper = options.wrapper;
+            carousel = options.carousel;
+            carouselContainer = options.carouselContainer;
             about = options.about;
             aboutLink = options.aboutLink;
             aboutClose = options.aboutClose;
-            ssGo = options.ssGo;
-            ssForm = options.ssForm;
+
+            templateMasonry = options.templateMasonry;
+            templateCarousel = options.templateCarousel;
+            templateTreatmentsFTS = options.templateTreatmentsFTS;
 
             if (location.search) {
                 getQueryParamsAndImages(location.search);
             }
 
-            aboutLink.addEventListener('click', Ocellus.toggleAbout);
-            aboutClose.addEventListener('click', Ocellus.toggleAbout);
-            ssGo.addEventListener('click', Ocellus.goGetItWrapper);
-            ssForm.addEventListener('submit', Ocellus.goGetItWrapper);
+            aboutLink.addEventListener('click', toggleAbout);
+            aboutClose.addEventListener('click', toggleAbout);
+            ssGo.addEventListener('click', goGetIt);
+            ssForm.addEventListener('submit', goGetIt);
+
+            for (let i = 0; i < resultTypeChooser.children.length; i++) {
+                resultTypeChooser.children[i].addEventListener('click', chooseResultType)
+            }
+
+            baseUrl = zenodeo + basePath;
         }
     }
 }());
