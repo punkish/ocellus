19f4b02226c045e911ca81ccad8f84a3c6ac5ff3
punkish <punk.kish@gmail.com>
2019-08-20 21:37:28 +0530

    removed config.js from git

--- js/config.js
+++
@@ -1,7 +0,0 @@
-const zenodeo = 'http://localhost:3030';
-
-if (typeof(OCELLUS) === 'undefined' || typeof(OCELLUS) !== 'object') {
-    OCELLUS = {};
-}
-
-OCELLUS.zenodeo = 'http://localhost:3030';
