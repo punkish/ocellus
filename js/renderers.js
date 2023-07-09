046777d7f9ce712097fc97eea5a7fdfc04e34965
punkish <punk.kish@gmail.com>
2023-07-09 12:09:36 +0200

    filled rect for legend

--- js/renderers.js
+++ js/renderers.js
@@ -115,7 +115,7 @@ const renderPage = (resultsObj) => {
 
     $('#grid-images').classList.add(`columns-${figureSize}`);
 
-    renderFigures(figures, qs, prev, next);
+    //renderFigures(figures, qs, prev, next);
     renderSearchCriteria(qs, count, cacheHit);
     $('#throbber').classList.add('nothrob');
 
@@ -271,36 +271,149 @@ const termFreqWithDygraphs = (ctx, width, height, series, term, termFreq) => {
 let termFreqChart;
 
 const termFreqWithChartjs = (ctx, width, height, series, term, termFreq) => {
+    const colors = {
+        red: 'rgba(255, 0, 0, 0.6)',
+        lightRed: 'rgba(255, 0, 0, 0.1)',
+        blue: 'rgba(0, 0, 255, 0.6)',
+        lightBlue: 'rgba(0, 0, 255, 0.1)',
+        black: 'rgba(0, 0, 0, 0.4)'
+    }
     
+    // series "total"
+    const total = {
+        label: series.y1,
+        data: termFreq.map(e => e.total),
+        borderColor: colors.red,
+        borderWidth: 1,
+        backgroundColor: colors.lightRed,
+        pointStyle: 'circle',
+        pointRadius: 3,
+        pointBorderColor: 'red'
+    };
+
+    const withImages = {
+        label: series.y2,
+        data: termFreq.map(e => e.withImages),
+        borderColor: colors.blue,
+        borderWidth: 1,
+        backgroundColor: colors.lightBlue,
+        pointStyle: 'circle',
+        pointRadius: 3,
+        pointBorderColor: 'blue'
+    };
+
+    const pluginLegendBackground = {
+        id: 'legendBackground',
+        beforeDraw({legend}){
+            // avoid useless drawing of the legend before the rectangle is drawn
+            this._draw = legend.draw;
+            legend.draw = ()=>null;
+        },
+        beforeUpdate({legend}){
+            legend.topChanged = false;
+        },
+        afterDraw({legend}, args, opts){
+            const {
+                options: {labels: {padding}},
+                legendHitBoxes,
+                ctx
+            } = legend;
+
+            let {top, bottom, left, right} = legendHitBoxes.reduce(
+                ({top, bottom, left, right}, {top: t, height: h, left: l, width: w})=>
+                    ({
+                        top: Math.min(top, t),
+                        bottom: Math.max(bottom, t+h),
+                        left: Math.min(left, l),
+                        right: Math.max(right, l+w)
+                    }), {top: 1/0, bottom: 0, left: 1/0, right: 0});
+
+            if (top < bottom && left < right) {
+                top -= padding.top ?? padding;
+                bottom += padding.bottom ?? padding;
+                left -= padding.left ?? padding;
+                right += padding.right ?? padding;
+    
+                const borderWidth = opts.borderWidth ?? 0;
+    
+                let deltaX = 0, deltaY = 0;
+
+                if (left - borderWidth <= 0) {
+                    deltaX = -left + borderWidth;
+                }
+                else if (right + borderWidth >= legend.chart.width) {
+                    deltaX = legend.chart.width - (right + borderWidth);
+                }
+
+                if (top - borderWidth < 0) {
+                    deltaY = - top + borderWidth;
+                }
+
+                if (bottom + borderWidth > legend.chart.height) {
+                    deltaY = legend.chart.height - bottom - borderWidth;
+                }
+
+                if (deltaX !== 0 || deltaY !== 0) {
+                    left += deltaX;
+                    right += deltaX;
+                    legendHitBoxes.forEach(lb => lb.left += deltaX)
+                    legend.left += deltaX;
+                }
+
+                if (deltaX !== 0 || deltaY !== 0) {
+                    top += deltaY;
+                    bottom += deltaY;
+
+                    if (!legend.topChanged) {
+                        legend.top += deltaY;
+                        legend.topChanged = true;
+                    }
+                }
+
+                left -= Math.floor(borderWidth/2);
+                right += Math.floor(borderWidth/2);
+                top -= Math.floor(borderWidth/2);
+                bottom += Math.floor(borderWidth/2);
+    
+                ctx.save();
+                ctx.fillStyle = opts.color ?? 'transparent';
+                ctx.lineWidth = opts.borderWidth ?? 0;
+                ctx.strokeStyle = opts.borderColor ?? 'black';
+                //ctx.fillRect(left, top, right - left, bottom - top);
+                //ctx.strokeRect(left, top, right - left, bottom - top);
+                ctx.roundRect(left, top, right - left, bottom - top, 5);
+                ctx.stroke();
+                ctx.fill();
+                ctx.restore();
+            }
+
+            legend.draw = this._draw;
+            delete this._draw;
+            legend._draw();
+        }
+    };
+
+    Chart.register(pluginLegendBackground);
+
+    // const plugin = {
+    //     id: 'legendBackground',
+    //     beforeDraw: (chart, args, opts) => {
+    //         const { chartArea: { width, top, left }, ctx } = chart;
+    //         ctx.fillStyle = opts.color || 'white';
+    //         ctx.fillRect(left, 0, width, top)
+    //     }
+    // }
+
     // for config details, see
     // https://stackoverflow.com/a/76636677/183692
     const config = {
         type: 'line',
         data: {
             labels: termFreq.map(e => e.journalYear),
-            datasets: [
-                {
-                    label: series.y1,
-                    data: termFreq.map(e => e.total),
-                    borderColor: 'rgba(255, 0, 0, 0.6)',
-                    borderWidth: 1,
-                    backgroundColor: 'rgba(255, 0, 0, 0.1)',
-                    pointStyle: 'circle',
-                    pointRadius: 3,
-                    pointBorderColor: 'red'
-                },
-                {
-                    label: series.y2,
-                    data: termFreq.map(e => e.withImages),
-                    borderColor: 'rgba(0, 0, 255, 0.6)',
-                    borderWidth: 1,
-                    backgroundColor: 'rgba(0, 0, 255, 0.1)',
-                    pointStyle: 'circle',
-                    pointRadius: 3,
-                    pointBorderColor: 'blue'
-                }
-            ]
+            datasets: [ total, withImages ]
         },
+        //plugins: [ plugin ],
+        plugins: [{legendBackground: pluginLegendBackground}],
         options: {
             interaction: {
                 intersect: false,
@@ -315,21 +428,14 @@ const termFreqWithChartjs = (ctx, width, height, series, term, termFreq) => {
                 y: {
                     display: true,
                     type: 'logarithmic',
-
                     grid: {
                       
                         // minor ticks not visible
-                        tickColor: (data) => data.tick.major 
-                          ? 'silver' 
-                          : '',
+                        tickColor: (data) => data.tick.major ? 'silver' : '',
                       
                         //lineWidth: (data) => data.tick.major ? 2 : 1,
                       
-                        color: (data) => data.tick.major 
-                          ? 'rgba(0, 0, 0, 0.4)' 
-                          : 'silver',
-                      
-                        //z: -100000
+                        color: (data) => data.tick.major ? colors.black : 'silver',
                     },
                     border: {
                       
@@ -342,11 +448,48 @@ const termFreqWithChartjs = (ctx, width, height, series, term, termFreq) => {
                     // this eliminates tick values like 15 or 150 and only keeps
                     // those of the form n*10^m with n, m one digit integers
                     // this might not be necessary
-                    afterBuildTicks: (ax) => {
-                        ax.ticks = ax.ticks.filter(({value})=>{
-                            const r = value / Math.pow(10, Math.floor(Math.log10(value)+1e-5));
-                            return Math.abs(r - Math.round(r)) < 1e-5
-                        })
+                    // afterBuildTicks: (ax) => {
+                    //     ax.ticks = ax.ticks.filter(({value}) => {
+                    //         const r = value / Math.pow(10, Math.floor(Math.log10(value)+1e-5));
+                    //         return Math.abs(r - Math.round(r)) < 1e-5
+                    //     })
+                    // },
+                    afterBuildTicks: function(ax) {
+
+                        // fraction of the previous major tick value,
+                        // use 1 to have 8 minor ticks between two major ones,
+                        // which is the usual
+                        const minorTickStepSize = 0.5;
+                        
+                        ax.ticks = ax.ticks
+                            .filter(({ major }) => major)
+                            .flatMap(({ value, major, significand }, i, a) => {
+                                if (i === 0) {
+                                    return [{ value, major, significand }]
+                                } 
+                                else {
+                                    const { value: prevValue } = a[i - 1];
+                                    const h = Math.abs(minorTickStepSize * prevValue);
+                                    const aMinor = [];
+                                    let minorValue = prevValue + h;
+
+                                    while (minorValue < value) {
+                                        aMinor.push({
+                                            value: minorValue,
+                                            major: false,
+                                            significand: minorValue //prevValue
+                                        });
+
+                                        minorValue += h;
+                                    }
+
+                                    return [
+                                        ...aMinor, 
+                                        { value, major, significand }
+                                    ];
+                                }
+                            }
+                        );
                     },
                     ticks: {
                         callback: function (value, index, ticks) {
@@ -359,9 +502,7 @@ const termFreqWithChartjs = (ctx, width, height, series, term, termFreq) => {
                             if (value === 1) return "1";
                             return '';
                         },
-                        //major: {
-                        //  enabled: true
-                        //}
+                        autoSkip: false
                     }
                 }
             },
@@ -375,11 +516,20 @@ const termFreqWithChartjs = (ctx, width, height, series, term, termFreq) => {
                     position: 'chartArea',
                     labels: {
                         usePointStyle: true,
-                    }
+                    },
+                    backgroundColor: 'rgba(0, 0, 255, 1)'
                 },
+                legendBackground:{
+					color: 'rgb(255,255,255)',
+					borderWidth: 1,
+					borderColor: 'black'
+				},
                 tooltip: {
                     enabled: true
-                }
+                },
+                // legendBackground: {
+                //     color: 'blue'
+                // }
             }
         }
     };
@@ -393,6 +543,7 @@ const termFreqWithChartjs = (ctx, width, height, series, term, termFreq) => {
     else {
         canvas = document.createElement('canvas');
         canvas.id = "termFreq";
+        console.log(`setting canvas dims to ${width} x ${height}`)
         canvas.width = width;
         canvas.height = height;
         ctx.appendChild(canvas);
