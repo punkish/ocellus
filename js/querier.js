b70f27f33b67d37cfb68c8178805d665ff68ccf1
punkish <punk.kish@gmail.com>
2026-06-19 17:06:26 +0200

    first pass

--- js/querier.js
+++ js/querier.js
@@ -73,8 +73,11 @@ const getResource = async (qs) => {
     // save page and size to use later to update the results
     const page = sp.get('page');
     const size = sp.get('size');
-    const grid = sp.get('grid') || 'normal';
-    const figureSize = globals.figureSize[grid];
+    const layoutEl = $('input[name=layout]');
+    const imgEl = $('input[name=img]');
+    const layout = layoutEl ? layoutEl.value : 'normal';
+    const imgSize = imgEl ? imgEl.value : (layout === 'pg' ? '50' : '250');
+    const figureSize = parseInt(imgSize, 10);
 
     // what are we getting, images or treatments?
     const resource = sp.get('resource');
@@ -187,6 +190,7 @@ const getResource = async (qs) => {
 
             const resultsObj = {
                 resource,
+                layout,
                 figureSize,
                 slides,
                 qs, 
